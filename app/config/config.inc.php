90fbac50e1ef7385caf18841fdf24617edfa46f0
Abdalla Fayez <Abdalla.Fayez@uofcanada.edu.eg>
2024-11-03 14:31:12 +0200

    Initial commit

---
+++ app/config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("PJ_HOST")) define("PJ_HOST", "localhost");
+if (!defined("PJ_USER")) define("PJ_USER", "root");
+if (!defined("PJ_PASS")) define("PJ_PASS", "");
+if (!defined("PJ_DB")) define("PJ_DB", "pj_busdb");
+if (!defined("PJ_PREFIX")) define("PJ_PREFIX", "");
+if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/pj_bus/");
+if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "C:/xampp/htdocs/PJ_bus/");
+if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://localhost/pj_bus/");
+if (!defined("PJ_SALT")) define("PJ_SALT", "GFTGKBWN");
+if (!defined("PJ_INSTALLATION")) define("PJ_INSTALLATION", "27F34753FA048AA3237CA920C4DEE02C84A80B15DF88BCC9E9B2A771468EF1A3");
+?>
