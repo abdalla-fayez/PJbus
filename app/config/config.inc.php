c202de4768490eeb48a5e217d277ee686d9f8cab
abdalla-fayez <af.ragheb98@gmail.com>
2024-11-05 11:52:43 +0200

    Update 1

--- app/config/config.inc.php
+++ app/config/config.inc.php
@@ -6,7 +6,7 @@ if (!defined("PJ_DB")) define("PJ_DB", "pj_busdb");
 if (!defined("PJ_PREFIX")) define("PJ_PREFIX", "");
 if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/pj_bus/");
 if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "C:/xampp/htdocs/PJ_bus/");
-if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://localhost/pj_bus/");
+if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://" . $_SERVER['HTTP_HOST'] . "/pj_bus/");
 if (!defined("PJ_SALT")) define("PJ_SALT", "GFTGKBWN");
 if (!defined("PJ_INSTALLATION")) define("PJ_INSTALLATION", "27F34753FA048AA3237CA920C4DEE02C84A80B15DF88BCC9E9B2A771468EF1A3");
 ?>
